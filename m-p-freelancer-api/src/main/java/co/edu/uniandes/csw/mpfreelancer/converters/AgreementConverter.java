4f06b7a7d6714be49deab27656ad3981cd0570be
jc.nino11 <jc.nino11@172.24.99.153>
2016-02-28 13:31:07 -0500

    Merge origin/master
    Conflicts:
    	.gitignore
    	.travis.yml
    	README.md
    	sonar-project.properties

--- m-p-freelancer-api/src/main/java/co/edu/uniandes/csw/mpfreelancer/converters/AgreementConverter.java
+++ m-p-freelancer-api/src/main/java/co/edu/uniandes/csw/mpfreelancer/converters/AgreementConverter.java
@@ -72,11 +72,7 @@ public abstract class AgreementConverter {
             entity.setSelected(dto.getSelected());
             entity.setFreelancer(FreelancerConverter.refDTO2Entity(dto.getFreelancer()));
             entity.setProject(ProjectConverter.refDTO2Entity(dto.getProject()));
-<<<<<<< OURS
-            
-=======
 
->>>>>>> THEIRS
             return entity;
         } else {
             return null;
